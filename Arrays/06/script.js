f6d788473583689f8f3d81325ef17a06ef517975
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-15 09:26:51 -0400

    Finding in array

---
+++ Arrays/06/script.js
@@ -0,0 +1,18 @@
+/**
+ * Tu desafío es crear un buscador usando funciones como filter y includes,
+ * vas a tener array con palabras e tienes que retornar un array con sola las que
+ * cumplan con la condición de tener el parámetro de búsqueda.
+ * La solución debería tener un input y output como los siguientes:
+ * solution(["ana", "santi", "nico", "anastasia"], "an");
+ * solution(["ana", "santi", "nico", "anastasia"], "xyz");
+ * Output:
+ * ["ana", "santi", "anastasia"]
+ * []
+ */
+ function solution(word, query) {
+  return word.filter(w => w.includes(query))  
+
+}; 
+console.log(solution(["ana", "santi", "nico", "anastasia"], "an"));
+console.log(solution(["ana", "santi", "nico", "anastasia"], "xyz"))
+
