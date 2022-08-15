6f3637fc855282a49722b461c1e68ee5b2d27877
Anibal Corral Serrano <anibal.corral@gmail.com>
2022-08-15 09:42:31 -0400

    Using some function to determine if it is even

---
+++ Arrays/08/script.js
@@ -0,0 +1,21 @@
+/**
+ * En este desafío tienes un array de números y debes retornar true si dentro
+ *  de los números de ese array al menos uno es un número par.
+ * La solución debería tener un input y output como los siguientes:
+ * solution([1, 3, 5, 7, 10, 11]);
+solution([1, 3, 5]);
+ * 
+ * Output
+
+true
+false
+ * 
+ */
+
+function solution(numbers) {
+  // Tu código aquí 👈 
+  return numbers.some((num)=>num%2===0)
+}; 
+
+console.log(solution([1, 3, 5, 7, 10, 11]));
+console.log(solution([1, 3, 5]));
